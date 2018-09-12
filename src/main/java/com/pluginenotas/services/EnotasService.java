1b5b12a40844dd0dc2d55630ed73ab14961fe6bc
esantiagovieira <esantiagovieira@gmail.com>
2018-09-12 16:16:46 -0300

    Melhorias e correção na emissão do json

--- src/main/java/com/pluginenotas/services/EnotasService.java
+++ src/main/java/com/pluginenotas/services/EnotasService.java
@@ -19,7 +19,9 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.pluginenotas.utils.MYSQLAccess;
 
@@ -84,123 +86,45 @@ public String enviarlogo (String idempresa ) throws ClientProtocolException, IOE
 		return null;
 	}
 	
-	public String enviarEmpresa(ObjectNode json) {
-		String idEmpresa=null;
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		HttpPost post = new HttpPost(incluirEmpresa);
-		post.setHeader("Content-Type", "application/json");
-		post.setHeader("Accept", "application/json");
-		post.setHeader("Authorization", "Basic " + apiKey);
-		try {
-			StringEntity entity = new StringEntity("");
-			post.setEntity((HttpEntity) json);
-			entity.setContentEncoding("UTF-8");
-			entity.setContentType("application/json");
-			System.out.println(json);
-			System.out.println("======================");
-			CloseableHttpResponse response = httpClient.execute(post);
-			System.out.println(response.getStatusLine().getStatusCode() + " - " + response.getStatusLine().getReasonPhrase());
-			idEmpresa = response.getEntity().getContent().toString();
-
-		}
-		catch (Exception e) {
-			throw new RuntimeException("Erro ao cadastrar empresa: " + e.getMessage());
-		}
-		return idEmpresa ;
-	}
-	
 	public String enviarEmpresa(String json)  {
 		String idEmpresa=null;
+		ObjectMapper mapper = new ObjectMapper();
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 		HttpPost post = new HttpPost(incluirEmpresa);
 		post.setHeader("Content-Type", "application/json");
 		post.setHeader("Accept", "application/json");
 		post.setHeader("Authorization", "Basic " + apiKey);
 		try {
-			StringEntity entity = new StringEntity(json);
+			StringEntity entity = new StringEntity(json, ContentType.APPLICATION_JSON);
 			post.setEntity(entity);
-			entity.setContentEncoding("UTF-8");
-			entity.setContentType("application/json");
+//			entity.setContentEncoding("UTF-8");
+//			entity.setContentType("application/json");
 			System.out.println(json);
 			System.out.println("======================");
 			CloseableHttpResponse response = httpClient.execute(post);
 			System.out.println(response.getStatusLine().getStatusCode() + " - " + response.getStatusLine().getReasonPhrase());
-			idEmpresa = response.getEntity().getContent().toString();
-
+			
+			JsonNode rootArray = mapper.readTree(response.getEntity().getContent());
+			for(JsonNode root : rootArray) {
+				if(root.has("idEmpresa")) {
+					idEmpresa = root.path("idEmpresa").asText();
+				}
+				else {
+					idEmpresa = root.path("mensagem").asText();
+				}
+			}
+			
 		}
 		catch (Exception e) {
 			throw new RuntimeException("Erro ao cadastrar empresa: " + e.getMessage());
 		}
 		return idEmpresa ;
-	}
-	
-	public String buildJsonEmpresa(Integer id) {
-		IbgeService ibge = new IbgeService();
-		ObjectMapper mapper = new ObjectMapper();
-		ObjectNode root = mapper.createObjectNode();
-		ResultSet dao = new MYSQLAccess().readDatabase(id);
-		
-		try {
-		Integer codigoIbgeUf =Integer.parseInt(ibge.buscaUFs(dao.getString("sigla_uf"))) ;
-		Integer codigoIbgeCidade = Integer.parseInt(ibge.buscaMunicipioPorUF(codigoIbgeUf, dao.getString("cidade")));
-		
-		root.putNull("id");
-		root.put("cnpj", dao.getString("cnpj"));
-		root.put("inscricaoMunicipal", dao.getString("insc_municipal"));
-		root.put("inscricaoEstadual", dao.getString("insc_estadual"));
-		root.put("razaoSocial", dao.getString("razao_social"));
-		root.put("nomeFantasia", dao.getString("nome_fantasia"));
-		
-		int crt = Integer.parseInt(dao.getString("crt"));
-		if(crt>1) {
-			root.put("optanteSimplesNacional", true);
-		}
-		else {
-			root.put("optanteSimplesNacional", true);
-			}
-		
-		root.put("email", dao.getString("email"));
-		root.put("telefoneComercial", dao.getString("ddd_telefone") + dao.getLong("telefone"));
-		root.put("incentivadorCultural", false);
-		root.put("regimeEspecialTributacao",dao.getString("cod_regime_tributacao_rps"));
-		root.put("codigoServicoMunicipal", "0101");
-		root.putNull("itemListaServicoLC116");
-		root.putNull("cnae");
-		root.put("aliquotaIss", 0.02);
-		root.put("descricaoServico","   ");
-		root.put("enviarEmailCliente",true);
-		
-		ObjectNode endereco = mapper.createObjectNode();
-		endereco.put("codigoIbgeUf", codigoIbgeUf);
-		endereco.put("codigoIbgeCidade", codigoIbgeCidade);
-		endereco.put("pais", dao.getString("pais"));
-		endereco.put("uf", dao.getString("sigla_uf"));
-		endereco.put("cidade", dao.getString("cidade"));
-		endereco.put("logradouro", dao.getString("endereco"));
-		endereco.put("numero", dao.getString("numero"));
-		endereco.put("complemento", dao.getString("complemento_end"));
-		endereco.put("bairro", dao.getString("bairro"));
-		endereco.put("cep", dao.getString("cep"));
-		
-		ObjectNode confProducao = mapper.createObjectNode();
-		confProducao.put("sequencialNFe", 1);
-		confProducao.put("serieNFe", 1);
-		confProducao.put("sequencialLoteNFe", 1);
-		
-		root.set("endereco", endereco);
-		root.set("ConfiguracoesNFSeHomologacao", confProducao);
-		root.set("ConfiguracoesNFSeProducao", confProducao);
-		}
-		catch (Exception e) {
-			throw new RuntimeException("Falha ao criar o json: ");
-		}
-		String idEmpresa = enviarEmpresa(root.toString());
-		return idEmpresa;
 	}
 	
 	public String buildJsonEmpresa(String host, String dbName, String user, String pass, String port, Integer id) {
 		IbgeService ibge = new IbgeService();
 		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
 		ObjectNode root = mapper.createObjectNode();
 		ResultSet dao = new MYSQLAccess().readDatabase(id, host,dbName,user, pass, port);
 		
@@ -251,6 +175,9 @@ public String enviarlogo (String idempresa ) throws ClientProtocolException, IOE
 		confProducao.put("sequencialNFe", 1);
 		confProducao.put("serieNFe", 1);
 		confProducao.put("sequencialLoteNFe", 1);
+		confProducao.putNull("usuarioAcessoProvedor");
+		confProducao.putNull("senhaAcessoProvedor");
+		confProducao.putNull("tokenAcessoProvedor");
 		
 		root.set("endereco", endereco);
 		root.set("ConfiguracoesNFSeHomologacao", confProducao);
@@ -259,8 +186,7 @@ public String enviarlogo (String idempresa ) throws ClientProtocolException, IOE
 		catch (Exception e) {
 			throw new RuntimeException("Falha ao criar o json: ");
 		}
-//		return root.toString();
-		String idEmpresa = enviarEmpresa(root.toString());
-		return idEmpresa;
+//		String idEmpresa = enviarEmpresa(root.toString());
+		return root.toString();
 	}
 }
